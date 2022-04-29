e184823d3ba0502e25692d72310fd60b016452e4
Qbe <jose.jtp@gmail.com>
2022-04-29 11:39:36 +0900

    Arena deck parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,37 @@
+val deckRatCar = """
+    4 Haunted Ridge (MID) 263
+    4 Greasefang, Okiba Boss (NEO) 220
+    4 Goblin Engineer (MH1) 128
+    2 Can't Stay Away (MID) 213
+    4 Faithless Looting (STA) 38
+    4 Parhelion II (WAR) 24
+    2 Wishclaw Talisman (ELD) 110
+    2 Needleverge Pathway (ZNR) 263
+    4 Blightstep Pathway (KHM) 252
+    4 Godless Shrine (RNA) 248
+    4 Brightclimb Pathway (ZNR) 259
+    4 Blood Crypt (RNA) 245
+    2 Sacred Foundry (GRN) 254
+    4 Bone Shards (MH2) 76
+    4 Stitcher's Supplier (M19) 121
+    4 Undead Butler (VOW) 133
+    2 Revival // Revenge (RNA) 228
+    1 Skysovereign, Consul Flagship (KLR) 272
+    1 Concealed Courtyard (KLR) 282
+""".trimIndent()
+
 fun main(args: Array<String>) {
     println("Test")
     //val searchQuery = readln()
-    val deckList = """
-        Greasefang, Okiba Boss 
-        Agadeem's Awakening
-        Parhelion II
-        Revival // Revenge 
-        Can't Stay Away 
-        Stitcher's Supplier 
-        Undead Butler 
-        Faithless Looting
-        Seasoned Pyromancer 
-        Goblin Engineer 
-        Wishclaw Talisman 
-        Bone Shards
-        Skysovereign, Consul Flagship 
-        Hive of the Eye Tyrant 
-        Haunted Ridge 
-        Savai Triome 
-        Blightstep Pathway 
-        Godless Shrine 
-        Brightclimb Pathway 
-        Blood Crypt 
-        Sacred Foundry 
-    """.trimIndent()
+    val deckList = deckRatCar
 
-    var cardList = forohfor.scryfall.api.MTGCardQuery.toCardList(deckList.split("\n","\r"), false)
+    val cardList = MTGCard.fromArenaList(deckList)
+
+            //var cardList = forohfor.scryfall.api.MTGCardQuery.toCardList(deckList.split("\n","\r"), false)
     //val queryResult = forohfor.scryfall.api.MTGCardQuery.search(searchQuery)
-    cardList?.map {
+    cardList.map {card ->
 
-        val card = MTGCard.of(it)
+        //val card = MTGCard.of(it)
         println(card.name + " " + card.types)
         println(card.cmc.toString() + " " + card.manaCost)
 
--- src/main/kotlin/scryfall.kt
+++ src/main/kotlin/scryfall.kt
@@ -1,3 +1,5 @@
+import forohfor.scryfall.api.MTGCardQuery
+
 fun MTGCard.Companion.of(scryfallCard:forohfor.scryfall.api.Card):MTGCard = if(scryfallCard.cardFaces == null) {
     MTGCard(
         name = scryfallCard.name,
@@ -22,3 +24,53 @@ fun MTGCard.Companion.of(scryfallFace:forohfor.scryfall.api.CardFace):MTGCard = 
     backside = null,
     json = scryfallFace.jsonData,
 )
+
+fun MTGCard.Companion.scryfallSearch(query:String):List<MTGCard> = MTGCardQuery.search(query)?.map { MTGCard.of(it) } ?: emptyList()
+
+//Receive a list of card names, one per line, for example:
+// Island
+// Mountain
+// Expressive Iteration
+//Split them all, trim them, and call forohfor.scryfall.api.MTGCardQuery.toCardList with the trimmed list
+//For each result, call MTGCard.Companion.of(scryfallCard) and return thant
+fun MTGCard.Companion.fromNameList(cardNames:String):List<MTGCard> {
+    val cardList = cardNames.split('\n','\r').map { it.trim() }.filter { it.isNotEmpty() }
+    val cards = MTGCardQuery.toCardList(cardList, false)
+    return cards.map { MTGCard.of(it) }
+}
+
+//Receive a list of Arena-style card names and their count, one for each line, for example:
+// 10 Island (NEO) 123
+// 10 Mountain (SNC) 234
+//Split them into individual lines
+//For each line, use a regular expression to extract the count, the card name, the set name between parentheses, and the card number
+//Use the card names to call MTGCard.of(cardNames)
+//For each returned card, copy it the amount of times specified in the count
+//Return the list of cards
+fun MTGCard.Companion.fromArenaList(arenaDeck:String):List<MTGCard> {
+    val cardList = arenaDeck.split('\n','\r').map { it.trim() }.filter { it.isNotEmpty() }
+    val cardNamesToAmount = cardList.map {
+        val regex = Regex("(\\d+) ([^\\(]+) \\(([^\\)]+)\\) (\\d+)")
+        val match = regex.find(it)
+        if(match != null) {
+            val count = match.groupValues[1].toInt()
+            val name = match.groupValues[2]
+            //val set = match.groupValues[3]
+            //val number = match.groupValues[4].toInt()
+            return@map name to count
+        } else {
+            return@map null
+        }
+    }.filterNotNull()
+    val cards = MTGCard.fromNameList(cardNamesToAmount.joinToString("\n") { it.first })
+    val deck = cards.flatMap { card ->
+        //Because of double-faced cards (like Revival // Revenge) we can't use an exact name match
+        //However, some double faced cards only have the front face as the name, so we can't use // for all of them
+        //So instead we do a partial match with the beginning of the name
+        val count = cardNamesToAmount.firstOrNull { it.first.startsWith(card.name) }?.second ?: 0
+        (1..count).map { card.copy() }
+    }
+    assert(deck.size == cardNamesToAmount.sumOf { it.second })
+    return deck
+}
+
